5fd5478efa8d94dffc3e7371e2bfcc547f0abf66
greenfrog616 <greenfrog0616@gmail.com>
2024-01-11 10:52:07 +0900

    🧪 :: Header

---
+++ src/pages/Main/MainIntro.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainHeader from "../../components/Header1/Header1";
+
+const MainIntro = () => {
+  return (
+    <>
+      <MainHeader />
+      {/* <div>Main_intro</div> */}
+    </>
+  );
+};
+
+export default MainIntro;
