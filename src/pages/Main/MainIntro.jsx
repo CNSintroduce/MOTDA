ac5b3e67dd73a969e5e8bf6f4d798bb98b5c968e
greenfrog616 <greenfrog0616@gmail.com>
2024-01-12 01:33:12 +0900

    ⚡ :: 글씨 추가

--- src/pages/Main/MainIntro.jsx
+++ src/pages/Main/MainIntro.jsx
@@ -6,7 +6,15 @@ const MainIntro = () => {
   return (
     <>
       <MainHeader />
-      <s.MainTop></s.MainTop>
+      <s.MainTop>
+        <s.CNSText>C are</s.CNSText>
+        <s.CNSText>N</s.CNSText>
+        <s.CNSText>S ervice</s.CNSText>
+        <s.CNSText2>
+          당신의 시작을 가치있게, <br />
+          입학 원서 접수 사이트를 개발하는 <s.CNS>CNS</s.CNS> 입니다.
+        </s.CNSText2>
+      </s.MainTop>
     </>
   );
 };
