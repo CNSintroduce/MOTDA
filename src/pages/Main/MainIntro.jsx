4155d904ad42855ca76e08ca23083de10286928c
greenfrog616 <greenfrog0616@gmail.com>
2024-01-12 03:50:09 +0900

    ⚡ :: 메인 페이지 업데이트
    일단 만들고 있긴한디 만들다보니 크기가 너무 애매하고 이상하긴함 디자인 이거 맞아?

--- src/pages/Main/MainIntro.jsx
+++ src/pages/Main/MainIntro.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import MainHeader from "../../components/Header1/Header1";
 import * as s from "../Main/MainIntro.style";
+import one from "../../../public/img/one1.svg";
+import one2 from "../../../public/img/one2.svg";
+import imsi from "../../../public/img/imsi.svg";
 
 const MainIntro = () => {
   return (
     <>
       <MainHeader />
+
       <s.MainTop>
-        <s.CNSText>C are</s.CNSText>
-        <s.CNSText>N</s.CNSText>
-        <s.CNSText>S ervice</s.CNSText>
+        <s.CNSText>
+          <s.CNS2>C</s.CNS2> are <br></br>
+          <s.CNS2>N </s.CNS2>
+          <br></br>
+          <s.CNS2>S</s.CNS2> ervice
+        </s.CNSText>
         <s.CNSText2>
           당신의 시작을 가치있게, <br />
           입학 원서 접수 사이트를 개발하는 <s.CNS>CNS</s.CNS> 입니다.
         </s.CNSText2>
       </s.MainTop>
+
+      <s.One1 src={one} alt="" />
+      <s.One2 src={one2} alt="" />
+      <s.imsi src={imsi} alt="" />
     </>
   );
 };
--- src/pages/Main/MainIntro.style.js
+++ src/pages/Main/MainIntro.style.js
@@ -1,25 +1,49 @@
 import styled from "styled-components";
 
 export const MainTop = styled.div`
-  width: 659px;
-  height: 816px;
+  display: flex;
+  flex-direction: column;
+
+  width: 50vw;
+  height: 95vh;
   background-color: #1a276d;
 `;
 export const CNSText = styled.p`
   font-size: 60px;
   font-family: Pretendard;
   font-weight: 300;
-  &::first-letter {
-    font-size: 70px;
-
-    font-weight: 600;
-  }
+  margin-left: 10vw;
+  margin-top: 10vw;
+  color: #fff;
 `;
 export const CNSText2 = styled.p`
+  color: #fff;
+  margin-top: 5vw;
+  margin-left: 10vw;
   font-size: 20px;
-  font-weight: 300;
+  font-weight: 400;
 `;
 export const CNS = styled.span`
+  color: #fff;
   font-size: 20px;
+  font-weight: 600;
+`;
+export const CNS2 = styled.span`
+  font-size: 70px;
   font-weight: 500;
+`;
+export const One1 = styled.img`
+  position: absolute;
+  left: 80vw;
+  top: 85vh;
+`;
+export const One2 = styled.img`
+  position: absolute;
+  top: 175vh;
+`;
+export const imsi = styled.img`
+  position: absolute;
+  left: 50vw;
+  top: 10vw;
+  transform: scaleX(1.6);
 `;
