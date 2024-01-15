577400ab28cfd009faa7a550163dc315d0eab602
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-01-15 13:44:45 +0900

    ⚙ ::	프로젝트 세팅

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from"./Footer.style.js";
+
+const Footer = () => {
+  return (
+    <S.FooterContainer>
+        <div>footer</div>
+    </S.FooterContainer>
+  )
+}
+
+export default Footer;
