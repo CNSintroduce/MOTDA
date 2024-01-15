af8caaa1bf204a8f8dff119c877bc0372db80dc3
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-01-15 12:31:18 +0900

    ⚡ ::	footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as S from "../Footer/Footer.style.js";
+
+const Footer = () => {
+  return (
+    <>
+    <S.footerContainer>
+
+    </S.footerContainer>
+    </>
+  )
+}
+
+export default Footer;
