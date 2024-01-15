51e9c8a003cc6af741b671256b49e03d3f4def77
je355 <chaeeehui@gmail.com>
2024-01-15 15:27:22 +0900

    .

--- src/pages/Offer/Offer.tsx
+++ src/pages/Offer/Offer.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
+import Header from "../../components/Header2/Header2"
+import * as S from "../Offer/Offer.style";
 
 const Offer = () => {
   return (
     <>
-      <div>Offer</div>
+      <Header/>
     </>
   );
 };
