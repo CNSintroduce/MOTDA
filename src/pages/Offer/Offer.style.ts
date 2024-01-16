d01a11421c60629ba059a3cf29f9c6ab80cce7e6
je355 <chaeeehui@gmail.com>
2024-01-16 09:44:00 +0900

    ⚡ :: offer page

---
+++ src/pages/Offer/Offer.style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ServiceMain = styled.img`
+    object-fit: cover;
+    width: 100%;
+    height: 92vh;
+    margin: 0;
+    display: block;
+`
+
+export const Service2 = styled.img`
+    margin: 0px;
+    object-fit: cover;
+    width: 100%;
+    display: block;
+`
--- src/pages/Offer/Offer.tsx
+++ src/pages/Offer/Offer.tsx
@@ -1,11 +1,28 @@
 import React, {useState} from "react";
+import {useMediaQuery} from 'react-responsive';
 import Header from "../../components/Header2/Header2"
 import * as S from "../Offer/Offer.style";
 
+
+import serviceMain from "../../assets/img/serviceMain.png"
+import service2nd from "../../assets/img/service2page.svg"
+
+
 const Offer = () => {
+    const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 1224 })
+    const isBigScreen = useMediaQuery({ minDeviceWidth: 1824 })
+    const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 })
+    const isTabletOrMobileDevice = useMediaQuery({ maxDeviceWidth: 1224 })
+    const isPortrait = useMediaQuery({ orientation: 'portrait' })
+    const isRetina = useMediaQuery({ minResolution: '2dppx' })
+        
   return (
     <>
       <Header/>
+      <>
+        <S.ServiceMain src={serviceMain} alt="error"/>
+        <S.Service2 src={service2nd} alt="error"/>
+      </>
     </>
   );
 };
