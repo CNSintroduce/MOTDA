8ad1545ee07ec69079d50131df477ffeda5bf5c1
je355 <chaeeehui@gmail.com>
2024-01-17 12:14:55 +0900

    ⚡ :: offer

---
+++ src/pages/Offer/Offer.style.ts
@@ -0,0 +1,109 @@
+import styled from "styled-components";
+
+export const ServiceMain = styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: 92vh;
+  margin: 0;
+  display: block;
+`;
+
+export const Service2 = styled.img`
+  margin: 0px;
+  object-fit: cover;
+  width: 100%;
+  display: block;
+`;
+
+export const Page2Img = styled.div`
+  position: relative;
+`;
+
+export const Page2div = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+
+    margin-left: 50%;
+    margin-top: 10%;
+`;
+
+export const Page2Text = styled.span`
+    color: #FFF;
+    text-align: right;
+    font-family: Pretendard;
+    font-size: 35px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: normal;
+`;
+
+export const Page2ddiv = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+
+    margin-left: 70%;
+    margin-top: 23%;
+`
+
+export const Page2text = styled.span`
+    color: #FFF;
+
+    text-align: right;
+    font-family: Inter;
+    font-size: 60px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+`
+
+export const Challenge = styled.span`
+    color: #1A276D;
+    font-family: Inter;
+    font-size: 60px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+`
+
+export const Navigate = styled.span`
+    color: #3043A9;
+    font-family: Inter;
+    font-size: 60px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+`
+
+export const Success = styled.span`
+    color: #375BB5;
+    font-family: Inter;
+    font-size: 60px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+`
+
+export const Page2dddiv = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+
+    margin-left: 59%;
+    margin-top: 50%;
+`
+
+export const Page2TexT = styled.span`
+    color: #FFF;
+
+    text-align: right;
+    font-family: Inter;
+    font-size: 26px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 170%;
+`
--- src/pages/Offer/Offer.tsx
+++ src/pages/Offer/Offer.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/Header2/Header2";
+import * as S from "../Offer/Offer.style";
+
+import serviceMain from "../../assets/img/serviceMain.png";
+import service2nd from "../../assets/img/service2page.svg";
 
 const Offer = () => {
   return (
-    <div>Offer</div>
-  )
-}
+    <>
+      <Header />
+      <>
+        <S.ServiceMain src={serviceMain} alt="error" />
+        <>
+          <S.Page2Img>
+            <S.Service2 src={service2nd} alt="error" />
+            <S.Page2div>
+              <S.Page2Text>
+                꿈을 이루기 위한 도전의 첫걸음을 함께합니다.
+              </S.Page2Text>
+            </S.Page2div>
+            <S.Page2ddiv>
+                <S.Page2text>
+                “<S.Challenge>Challenge</S.Challenge>, 
+                <br/> <S.Navigate>Navigate</S.Navigate> 
+                <br/> and <S.Success>Success</S.Success>"
+                </S.Page2text>
+            </S.Page2ddiv>
+            <S.Page2dddiv>
+                <S.Page2TexT>
+                    CNS팀은 도전하는 개발자를 사랑하고 <br/>
+                    길잡이가 되어주는 개발자를 응원하고 <br/>
+                    여러분이 성공을 즐기는 개발자가 되기를 바랍니다.
+                </S.Page2TexT>
+            </S.Page2dddiv>
+          </S.Page2Img>
+        </>
+      </>
+    </>
+  );
+};
 
-export default Offer 
+export default Offer;
