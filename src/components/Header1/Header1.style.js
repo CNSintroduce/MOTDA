3215b3b10e58093941cfc3866c703926381fc6ea
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-01-20 15:16:16 +0900

    ⚡ :: Header1 높이 수정

--- src/components/Header1/Header1.style.js
+++ src/components/Header1/Header1.style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const MainHeader = styled.div`
   display: flex;
   width: 100%;
-  height: 50px;
+  height: 40px;
   justify-content: flex-end;
   align-items: center;
 `;
