13fc0b4b70474fdd4fc85fa563b6f87c4c23fb33
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-01-27 20:17:05 +0900

    ⚡ :: footer 구현 완료

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import * as S from "../Footer/Footer.style.js";
+import footerLogo from "../../assets/img/footerLogo.svg";
+import sendIcon from "../../assets/img/sendIcon.svg";
 
 const Footer = () => {
   return (
     <>
     <S.footerContainer>
-
+      <S.infoContainer>
+        <img src={footerLogo} alt='' />
+        <S.cns><b>C</b>are <b>N</b> <b>S</b>ervice</S.cns>
+        <S.cnsInfo>
+          <p>
+            CNS .Daegu Software Meister High School since 2019
+            <S.mail>이메일 | cnsofficial2019@gmail.com</S.mail>
+          </p>
+        </S.cnsInfo>
+      </S.infoContainer>
+      <hr />
+      <S.sendMent>
+        <S.p>
+          <S.content>Contact us</S.content>
+          <S.content><img src={sendIcon} alt='' /></S.content>
+        </S.p>
+      </S.sendMent>
     </S.footerContainer>
     </>
   )
