d198b8e9458940253cfd17c6cdde9fa59296867d
je355 <chaeeehui@gmail.com>
2024-02-01 10:45:29 +0900

    ⚡ :: header1 router

--- src/components/Header1/Header1.jsx
+++ src/components/Header1/Header1.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import { useNavigate, useLocation } from 'react-router-dom';
+
 import * as S from "../Header1/Header1.style.js";
 
 const MainHeader = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const isMain = location.pathname === "/";
+  const isIntroduce = location.pathname === "/introduction";
+  const isService = location.pathname === "/offer";
+  const isQna = location.pathname === "/qna";
+  
+
   return (
       <S.Container>
         <S.Context>
-          <p>메인</p>
-          <p>소개</p>
-          <p>서비스</p>
-          <p>문의</p>
+          <p onClick={() => navigate("/")}>메인</p>
+          <p onClick={() => navigate("/introduction")}>소개</p>
+          <p onClick={() => navigate("/offer")}>서비스</p>
+          <p onClick={() => navigate("/qna")}>문의</p>
         </S.Context>
       </S.Container>
   );
