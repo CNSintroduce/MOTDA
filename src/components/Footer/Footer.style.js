9fa9884aa44d8bfa50b6a4d34531d6921e9dcd3f
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-02-05 14:11:01 +0900

    ⚡ ::	기능 등 업데이트

--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-<<<<<<< HEAD
 
 export const footerContainer = styled.div`
 background-color: #010335;
@@ -55,8 +54,4 @@ export const content = styled.span`
 margin: 0;
 display: flex;
 align-items: flex-end;
-=======
-export const FooterContainer = styled.div`
-  background-color: red;
->>>>>>> 5a5966b630930fac1d110934f66868ec921854f3
 `;
